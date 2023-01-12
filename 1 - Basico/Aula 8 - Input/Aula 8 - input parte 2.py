d38f090f36146fa09e3eb605cf4199a27e298b20
Lucasacademico <100856355+Lucasacademico@users.noreply.github.com>
2023-01-12 15:26:24 -0400

    Update Aula 8 - input parte 2.py

--- 1 - Basico/Aula 8 - Input/Aula 8 - input parte 2.py
+++ 1 - Basico/Aula 8 - Input/Aula 8 - input parte 2.py
@@ -8,8 +8,9 @@ numero_1 = input('Digite um número: ')
 numero_2 = input('Digite outro número: ')
 print(int(numero_1) + int(numero_2))
 #  mudando o tipo de string para Inteiro no ato da soma
-#  Podemos também mudar o tipo diretamente no input:
+
 '''
+Podemos também mudar o tipo diretamente no input:
 ex:
     numero_1 = int(input('Digite um número)) - 
     neste código mudando o tipo diretamente no input
